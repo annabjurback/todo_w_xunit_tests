de2723e51100ffd4ee2ff0cf67eaf65515b03a5e
annabjurback <annabjurback@gmail.com>
2023-12-13 09:45:40 +0100

    add todo class

---
+++ TodoApp_w_xUnit/TodoApp/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Models
+{
+	public class Todo
+	{
+		public int ID { get; set; }
+		public string? Title { get; set; }
+		public string? Description { get; set; }
+		public bool IsDone { get; set; }
+	}
+}
