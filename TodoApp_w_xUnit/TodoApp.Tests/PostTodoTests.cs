bd9635651f784dbfcde8bd8355ebe3af8078ae18
annabjurback <annabjurback@gmail.com>
2023-12-17 15:53:15 +0100

    post todo test consecutive IDs

--- TodoApp_w_xUnit/TodoApp.Tests/PostTodoTests.cs
+++ TodoApp_w_xUnit/TodoApp.Tests/PostTodoTests.cs
@@ -19,13 +19,41 @@ namespace TodoApp.Tests
 
             // Assert
             var result = response as ObjectResult;
-            Assert.Equal(200, result.StatusCode);
+            Assert.Equal(200, result!.StatusCode);
             Assert.IsType<List<Todo>>(result.Value);
 
             var todos = result.Value as List<Todo>;
-            Assert.Equal(title, todos[0].Title);
+            Assert.Equal(title, todos![0].Title);
             Assert.Equal(description, todos[0].Description);
             Assert.Equal(1, todos[0].ID);
+        }
+
+        [Fact]
+        public void Should_Return_List_Of_Todos_With_Consecutive_Ids()
+        {
+            //Arrange
+            var controller = new TodoController(new DataStorage());
+            string title = "title";
+
+            //Act
+            int x = 5;
+            for (int i = 1; i < x; i++)
+            {
+                controller.PostTodo(title, null);
+            }
+            var response = controller.PostTodo(title, null);
+
+            //Assert
+            var result = response as ObjectResult;
+            Assert.Equal(200, result!.StatusCode);
+            Assert.IsType<List<Todo>>(result.Value);
+
+            var todos = result.Value as List<Todo>;
+            Assert.Equal(x, todos!.Count);
+            for (int i = 1; i <= todos.Count; i++)
+            {
+                Assert.Equal(i, todos[i - 1].ID);
+            }
         }
     }
 }
