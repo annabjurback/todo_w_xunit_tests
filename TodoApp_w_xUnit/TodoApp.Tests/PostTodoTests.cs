43d4248781b6c6c272248cc9bee4438ec88734b7
annabjurback <annabjurback@gmail.com>
2023-12-17 14:00:55 +0100

    successful post todos test

---
+++ TodoApp_w_xUnit/TodoApp.Tests/PostTodoTests.cs
@@ -0,0 +1,31 @@
+﻿using TodoApp.Models;
+using TodoApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Tests
+{
+    public class PostTodoTests
+    {
+        [Fact]
+        public void Should_Return_List_With_Same_Data_As_Request()
+        {
+            //Arrange
+            var controller = new TodoController(new DataStorage());
+            string title = "Title";
+            string description = "Description";
+
+            // Act
+            var response = controller.PostTodo(title, description);
+
+            // Assert
+            var result = response as ObjectResult;
+            Assert.Equal(200, result.StatusCode);
+            Assert.IsType<List<Todo>>(result.Value);
+
+            var todos = result.Value as List<Todo>;
+            Assert.Equal(title, todos[0].Title);
+            Assert.Equal(description, todos[0].Description);
+            Assert.Equal(1, todos[0].ID);
+        }
+    }
+}
