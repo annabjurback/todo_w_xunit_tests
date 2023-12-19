67f99ab1e1211f01e29192916a33b5bf76918fbe
annabjurback <annabjurback@gmail.com>
2023-12-19 18:09:46 +0100

    first test for DeleteTodo()

---
+++ TodoApp_w_xUnit/TodoApp.Tests/DeleteTodoTests.cs
@@ -0,0 +1,33 @@
+﻿using TodoApp.Models;
+using TodoApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Tests
+{
+    public class DeleteTodoTests
+    {
+        [Fact]
+        public void ShouldReturn_Ok_And_StorageSize_ShouldBeLess()
+        {
+            // Arrange
+            var storage = new DataStorage();
+            var controller = new TodoController(storage);
+            string title = "Mock title";
+            int x = 5;
+            for (int i = 1; i <= x; i++)
+            {
+                controller.PostTodo(title, null);
+            }
+
+            // Act
+            var response = controller.DeleteTodo(x.ToString());
+
+            //Assert
+            var result = response as StatusCodeResult;
+            Assert.Equal(200, result!.StatusCode);
+            Assert.Equal(x-1, storage.Todos.Count);
+
+
+        }
+    }
+}
