f9fb4ef2435dde003aa0b669f77b2af88e18b93d
annabjurback <annabjurback@gmail.com>
2023-12-22 13:19:38 +0100

    test of DeleteTodo if param is not int

--- TodoApp_w_xUnit/TodoApp.Tests/DeleteTodoTests.cs
+++ TodoApp_w_xUnit/TodoApp.Tests/DeleteTodoTests.cs
@@ -4,28 +4,56 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TodoApp.Tests
 {
-    public class DeleteTodoTests
-    {
-        [Fact]
-        public void ShouldReturn_Ok_And_StorageSize_ShouldBeLess()
-        {
-            // Arrange
-            var storage = new DataStorage();
-            var controller = new TodoController(storage);
-            string title = "Mock title";
-            int x = 5;
-            for (int i = 1; i <= x; i++)
-            {
-                controller.PostTodo(title, null);
-            }
+	public class DeleteTodoTests
+	{
+		string title = "Mock title";
+		[Fact]
+		public void ShouldReturn_Ok_And_StorageSize_ShouldBeLess()
+		{
+			// Arrange
+			var storage = new DataStorage();
+			var controller = new TodoController(storage);
+			int x = 5;
+			string xString = x.ToString();
+			for (int i = 1; i <= x; i++)
+			{
+				controller.PostTodo(title, null);
+			}
 
-            // Act
-            var response = controller.DeleteTodo(x.ToString());
+			// Act
+			var response = controller.DeleteTodo(xString);
 
-            //Assert
-            var result = response as StatusCodeResult;
-            Assert.Equal(200, result!.StatusCode);
-            Assert.Equal(x-1, storage.Todos.Count);
-        }
-    }
+			//Assert
+			var result = response as StatusCodeResult;
+			Assert.Equal(200, result!.StatusCode);
+			Assert.Equal(x - 1, storage.Todos.Count);
+		}
+
+		[Fact]
+		public void ShouldReturn_ArgumentNullExeption_If_RequestIsNull()
+		{
+			// Arrange
+			var storage = new DataStorage();
+			var controller = new TodoController(storage);
+
+			//Assert (with built-in Act)
+			Assert.Throws<ArgumentNullException>(() => controller.DeleteTodo(null!));
+		}
+
+		[Fact]
+		public void ShouldReturn_BadRequest_If_RequestParam_CannotBeConvertedTo_Int()
+		{
+			// Arrange
+			var storage = new DataStorage();
+			var controller = new TodoController(storage);
+			controller.PostTodo(title, null);
+
+			//Act
+			var response = controller.DeleteTodo("hello");
+
+			// Assert
+			var result = response as StatusCodeResult;
+			Assert.Equal(400, result!.StatusCode);
+		}
+	}
 }
