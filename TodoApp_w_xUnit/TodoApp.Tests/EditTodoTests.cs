8eaf5671fa957a719069181fceedff3b8eff2e11
annabjurback <annabjurback@gmail.com>
2023-12-27 15:54:08 +0100

    test null request

--- TodoApp_w_xUnit/TodoApp.Tests/EditTodoTests.cs
+++ TodoApp_w_xUnit/TodoApp.Tests/EditTodoTests.cs
@@ -6,19 +6,20 @@ namespace TodoApp.Tests
 {
 	public class EditTodoTests
 	{
+		readonly string title = "title";
+		readonly string description = "description";
+		readonly string newTitle  = "new title";
+		readonly string newDescription = "new description";
 		[Fact]
 		public void ShouldReturn_Ok_AndStorageShouldBe_Updated()
 		{
 			//Arrange
 			var storage = new DataStorage();
 			var controller = new TodoController(storage);
-			controller.PostTodo("title", "description");
-			string idToUpdate = "1";
+			controller.PostTodo(title, "description");
 
 			//Act
-			string newTitle = "new title";
-			string newDescription = "new description";
-			var response = controller.EditTodo(idToUpdate,newTitle,newDescription);
+			var response = controller.EditTodo("1",newTitle,newDescription);
 
 			//Assert
 			var result = response as StatusCodeResult;
@@ -27,6 +28,18 @@ namespace TodoApp.Tests
 			Assert.Equal(newDescription, storage.Todos[0].Description);
 		}
 
+		[Fact]
+		public void ShouldReturn_BadRequest_If_AllParametersAreNull()
+		{
+			// Arrange
+			var storage = new DataStorage();
+			var controller = new TodoController(storage);
+			controller.PostTodo(title, description);
 
+			//Act and assert
+			Assert.Throws<ArgumentNullException>(() => controller.EditTodo(null!, null!, null!));
+			Assert.Throws<ArgumentNullException>(() => controller.EditTodo("1", null!, null!));
+			Assert.Throws<ArgumentNullException>(() => controller.EditTodo(null!, newTitle, newDescription));
+		}
 	}
 }
